6efa6091f59f9810631572f594ca6206bf68913f
balista-yu <yuuykeek@gmail.com>
2025-01-03 17:54:09 +0900

    Spring Initializrで作成したアプリケーションを追加

---
+++ backend/src/main/kotlin/com/book/manager/sample_book_manager/SampleBookManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.book.manager.sample_book_manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SampleBookManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<SampleBookManagerApplication>(*args)
+}
