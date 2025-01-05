fe6b0e56a6cf42d52d0cd40b80408e48b9efd618
balista-yu <yuuykeek@gmail.com>
2025-01-05 11:14:32 +0900

    BookWithRentalRecordの作成

---
+++ backend/src/main/kotlin/com/book/manager/sample_book_manager/infrastructure/database/record/custom/BookWithRentalRecord.kt
@@ -0,0 +1,13 @@
+package com.book.manager.sample_book_manager.infrastructure.database.record.custom
+
+import java.time.LocalDateTime
+
+data class BookWithRentalRecord(
+    var id: Int? = null,
+    var title: String? = null,
+    var author: String? = null,
+    var releaseDate: LocalDateTime? = null,
+    var userId: Int? = null,
+    var rentalDatetime: LocalDateTime? = null,
+    var returnDeadline: LocalDateTime? = null
+)
