b7d40408270d75932a13a329246931250db4f80c
balista-yu <yuuykeek@gmail.com>
2025-01-13 19:13:14 +0900

    fix db

--- infra/docker/postgres/init/init.sql
+++ infra/docker/postgres/init/init.sql
@@ -2,6 +2,25 @@ DO
 $$
 BEGIN
     IF
+EXISTS (SELECT FROM pg_database WHERE datname = 'sample_book_manager') THEN
+        PERFORM pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'sample_book_manager';
+        DROP
+DATABASE sample_book_manager;
+END IF;
+END
+$$;
+
+CREATE DATABASE sample_book_manager
+    WITH OWNER = 'test'
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'ja_JP.UTF-8'
+    LC_CTYPE = 'ja_JP.UTF-8'
+    TEMPLATE = template0;
+
+DO
+$$
+BEGIN
+    IF
 EXISTS (SELECT FROM pg_database WHERE datname = 'sample_book_manager_test') THEN
         PERFORM pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'sample_book_manager_test';
         DROP
