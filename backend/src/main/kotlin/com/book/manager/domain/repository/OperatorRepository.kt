226c475474c0f69d58268b3b34ac7fde91d643d7
balista-yu <yuuykeek@gmail.com>
2025-02-15 15:01:11 +0900

    NamedParameterJdbcTemplateを使いCriteriaを使ってSQLをまとめるようにリファクタ

--- backend/src/main/kotlin/com/book/manager/domain/repository/OperatorRepository.kt
+++ backend/src/main/kotlin/com/book/manager/domain/repository/OperatorRepository.kt
@@ -1,9 +1,8 @@
 package com.book.manager.domain.repository
 
+import com.book.manager.domain.criteria.OperatorCriteria
 import com.book.manager.domain.model.entity.Operator
-import com.book.manager.domain.model.id.OperatorId
 
 interface OperatorRepository {
-    fun find(email: String): Operator?
-    fun find(id: OperatorId): Operator?
+    fun find(criteria: OperatorCriteria): Operator?
 }
--- backend/src/main/kotlin/com/book/manager/infrastructure/repository/OperatorRepositoryImpl.kt
+++ backend/src/main/kotlin/com/book/manager/infrastructure/repository/OperatorRepositoryImpl.kt
@@ -1,24 +1,36 @@
 package com.book.manager.infrastructure.repository
 
+import com.book.manager.domain.criteria.OperatorCriteria
 import com.book.manager.domain.model.entity.Operator
-import com.book.manager.domain.model.id.OperatorId
 import com.book.manager.domain.repository.OperatorRepository
 import com.book.manager.infrastructure.hydrator.OperatorHydrator
-import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Repository
 
 @Repository
 class OperatorRepositoryImpl(
-    private val jdbcTemplate: JdbcTemplate,
+    private val jdbcTemplate: NamedParameterJdbcTemplate,
     private val operatorHydrator: OperatorHydrator,
 ) : OperatorRepository {
-    override fun find(email: String): Operator? {
-        val sql = "SELECT id, email, password, name, role_type FROM operator WHERE email = ?;"
-        return jdbcTemplate.query(sql, { rs, _ -> operatorHydrator.hydrate(rs) }, email).firstOrNull()
-    }
+    override fun find(criteria: OperatorCriteria): Operator? {
+        val sql = StringBuilder("SELECT id, email, password, name, role_type FROM operator")
+        val params = mutableMapOf<String, Any>()
 
-    override fun find(id: OperatorId): Operator? {
-        val sql = "SELECT id, email, password, name, role_type FROM operator WHERE id = ?;"
-        return jdbcTemplate.query(sql, { rs, _ -> operatorHydrator.hydrate(rs) }, id.value).firstOrNull()
+        if (criteria.id != null) {
+            sql.append(" WHERE id = :id")
+            params["id"] = criteria.id.value
+        }
+
+        if (criteria.email != null) {
+            if (params.isNotEmpty()) {
+                sql.append(" AND")
+            } else {
+                sql.append(" WHERE")
+            }
+            sql.append(" email = :email")
+            params["email"] = criteria.email
+        }
+
+        return jdbcTemplate.query(sql.toString(), params) { rs, _ -> operatorHydrator.hydrate(rs) }.firstOrNull()
     }
 }
